4799aebd26c7ac2b71aafb86be6208c7282aa91a
rkarumba <rkarumba@outlook.com>
2023-05-21 16:57:19 -0700

    Initial Commit

---
+++ Scrumdinger App/Scrumdinger_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scrumdinger_AppApp.swift
+//  Scrumdinger App
+//
+//  Created by Raymond's Mac on 5/21/23.
+//
+
+import SwiftUI
+
+@main
+struct Scrumdinger_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
