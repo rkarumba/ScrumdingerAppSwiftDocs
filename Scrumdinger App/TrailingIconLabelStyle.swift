ff476da6bf66e2c688b81718d4675f93c04da686
rkarumba <rkarumba@outlook.com>
2023-05-21 20:38:09 -0700

    Initial Commit

---
+++ Scrumdinger App/TrailingIconLabelStyle.swift
@@ -0,0 +1,21 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger App
+//
+//  Created by Raymond's Mac on 5/21/23.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
